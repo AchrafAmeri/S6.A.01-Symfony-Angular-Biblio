40e083d13b44950294d48cb4a9f3a1f41d22c7ba
Datpanda <davidtran31770@gmail.com>
2026-03-08 11:37:13 +0100

    fix: Update environment configuration for development and production

--- front_articles/src/environments/environment.development.ts
+++ front_articles/src/environments/environment.development.ts
@@ -1,7 +1,15 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:8008/api',
-  ssoUrl: 'https://localhost:8008/sso/to-symfony',
-  logoutUrl: 'https://localhost:8008/logout',
+  apiUrl: 'http://localhost:8008/api',
+  ssoUrl: 'http://localhost:8008/sso/to-symfony',
+  logoutUrl: 'http://localhost:8008/logout',
   frontUrl: 'http://localhost:4200'
 };
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://localhost:8008/api',
+//   ssoUrl: 'https://localhost:8008/sso/to-symfony',
+//   logoutUrl: 'https://localhost:8008/logout',
+//   frontUrl: 'http://localhost:4200'
+// };
--- front_articles/src/environments/environment.ts
+++ front_articles/src/environments/environment.ts
@@ -1,3 +1,11 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://localhost:8008/api',
+//   ssoUrl: 'http://localhost:8008/sso/to-symfony',
+//   logoutUrl: 'http://localhost:8008/logout',
+//   frontUrl: 'http://localhost:4200'
+// };
+
 export const environment = {
   production: true,
   apiUrl: 'https://localhost:8008/api',
